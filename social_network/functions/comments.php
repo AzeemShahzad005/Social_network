93683cdc1f7f5fb4948ea45fd129a174108131ed
Azeem Shahzad <49363483+AzeemShahzad005@users.noreply.github.com>
2019-11-11 10:47:35 +0500

    Update comments.php

--- social_network/functions/comments.php
+++ social_network/functions/comments.php
@@ -3,6 +3,7 @@ $get_id = $_GET['post_id'];
 				$get_com = "select * from comments where post_id='$get_id' ORDER by 1 DESC";
 		$run_com = mysqli_query($con,$get_com);
         
+
         while($row=mysqli_fetch_array($run_com)){
         $con = $row['comment'];
 		$com_name = $row['comment_author'];
