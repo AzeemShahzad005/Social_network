8877afda0ebd33666902f5032c1be9bb2e78ef2a
Azeem Shahzad <49363483+AzeemShahzad005@users.noreply.github.com>
2019-11-11 10:44:01 +0500

    Update pagination.php

--- social_network/functions/pagination.php
+++ social_network/functions/pagination.php
@@ -1,9 +1,9 @@
 <?php
 $query = "select * from posts";
 $result = mysqli_query($con,$query);
-//count the total records
+//Count the total Records
 $total_posts = mysqli_num_rows($result);
-//Using ceil function to divide the total records on per page
+//Using ceil Function to divide the total Records on Per Page
 $total_pages = ceil($total_posts / $per_page);
 //Going to First page 
 echo "
@@ -14,7 +14,7 @@ echo "
 for ($i=1; $i<=$total_pages; $i++){
 	echo "<a href='home.php?page=$i'>$i</a>";
 	}
-//Going for Last page
+//Going For Last Page
 echo  "<a href='home.php?page=$total_pages'>Last Page</a></center></div>";	
 
 
